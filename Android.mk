6b7fda54cd4a9796fa20b2ced521810ce6b14484
Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>
2019-05-17 19:30:48 +0300

    Android.mk: Build rules transformation due Android Q requirements
    Jira-Id: ADM-3213
    
    Signed-off-by: Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>
    Change-Id: Ibbbdbafdadaed6313411c6d13858e4532ebf78c3

--- Android.mk
+++ Android.mk
@@ -15,42 +15,42 @@
 
 # Include only for Renesas ones.
 ifneq (,$(filter $(TARGET_PRODUCT), salvator ulcb kingfisher))
-LOCAL_PATH:= $(call my-dir)
+
+LOCAL_PATH := $(call my-dir)
+TA_OEMLOCK_SRC       := $(LOCAL_PATH)/ta
+TA_OEMLOCK_UUID      := be1e65f4-40ca-11e9-b210d663bd873d93
 
 ################################################################################
 # Build OemLock HAL                                                            #
 ################################################################################
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.hardware.oemlock@1.0-service.renesas
-LOCAL_INIT_RC := android.hardware.oemlock@1.0-service.renesas.rc
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_REQUIRED_MODULES := be1e65f4-40ca-11e9-b210d663bd873d93.ta
-LOCAL_CFLAGS = \
-	-Wall \
-	-Werror \
-	-DANDROID_BUILD
-
-LOCAL_C_INCLUDES := \
-	vendor/renesas/utils/optee-client/public \
-	$(LOCAL_PATH)/ta
+LOCAL_MODULE                := android.hardware.oemlock@1.0-service.renesas
+LOCAL_INIT_RC               := android.hardware.oemlock@1.0-service.renesas.rc
+LOCAL_MODULE_RELATIVE_PATH  := hw
+LOCAL_MODULE_TAGS           := optional
+LOCAL_PROPRIETARY_MODULE    := true
+LOCAL_REQUIRED_MODULES      := $(TA_OEMLOCK_UUID)
+LOCAL_CFLAGS                += -DANDROID_BUILD
 
 LOCAL_SRC_FILES := \
-	service.cpp \
-	OemLock.cpp
+    service.cpp \
+    OemLock.cpp
+
+LOCAL_C_INCLUDES := \
+    vendor/renesas/utils/optee-client/public \
+    $(TA_OEMLOCK_SRC)
 
 LOCAL_SHARED_LIBRARIES := \
-	libteec \
-	libbase \
-	liblog \
-	libcutils \
-	libhardware \
-	libhidlbase \
-	libhidltransport \
-	libutils \
-	android.hardware.oemlock@1.0
+    libteec \
+    libbase \
+    liblog \
+    libcutils \
+    libhardware \
+    libhidlbase \
+    libhidltransport \
+    libutils \
+    android.hardware.oemlock@1.0
 
 include $(BUILD_EXECUTABLE)
 
@@ -60,26 +60,26 @@ include $(BUILD_EXECUTABLE)
 
 # Please keep this variable consistent with TA_OEMLOCK_UUID defined
 # in oemlock_ta.h
-TA_OEMLOCK_UUID := be1e65f4-40ca-11e9-b210d663bd873d93
-TA_OEMLOCK_SRC := $(LOCAL_PATH)/ta
-
-TA_OEMLOCK_OUT := $(TA_OUT_INTERMEDIATES)/$(TA_OEMLOCK_UUID)_OBJ
-
-TA_OEMLOCK_TARGET := $(TA_OEMLOCK_UUID)_ta
-
+TA_OEMLOCK_OBJ              = $(PRODUCT_OUT)/obj/TA_OBJ/$(TA_OEMLOCK_UUID)
+TA_OEMLOCK_OUT              = $(abspath $(TA_OEMLOCK_OBJ))
+TA_OEMLOCK_BINARY           = $(TA_OEMLOCK_OBJ)/$(TA_OEMLOCK_UUID).ta
 # OP-TEE Trusted OS is dependency for TA
-.PHONY: TA_OUT_$(TA_OEMLOCK_UUID)
-TA_OUT_$(TA_OEMLOCK_UUID): tee.bin
+OPTEE_BINARY                = $(PRODUCT_OUT)/obj/OPTEE_OBJ/core/tee.bin
+OPTEE_TA_DEV_KIT_DIR        = $(abspath $(PRODUCT_OUT)/obj/OPTEE_OBJ/export-ta_arm64)
+
+$(TA_OEMLOCK_BINARY): $(OPTEE_BINARY)
 	mkdir -p $(TA_OEMLOCK_OUT)
-	mkdir -p $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/optee_armtz
+	CROSS_COMPILE=$(BSP_GCC_CROSS_COMPILE) BINARY=$(TA_OEMLOCK_UUID) TA_DEV_KIT_DIR=$(OPTEE_TA_DEV_KIT_DIR) $(ANDROID_MAKE) -C $(TA_OEMLOCK_SRC) O=$(TA_OEMLOCK_OUT) clean
+	CROSS_COMPILE=$(BSP_GCC_CROSS_COMPILE) BINARY=$(TA_OEMLOCK_UUID) TA_DEV_KIT_DIR=$(OPTEE_TA_DEV_KIT_DIR) $(ANDROID_MAKE) -C $(TA_OEMLOCK_SRC) O=$(TA_OEMLOCK_OUT) all
 
-.PHONY: $(TA_OEMLOCK_TARGET)
-$(TA_OEMLOCK_TARGET): TA_OUT_$(TA_OEMLOCK_UUID)
-	CROSS_COMPILE=$(OPTEE_CROSS_COMPILE) BINARY=$(TA_OEMLOCK_UUID) TA_DEV_KIT_DIR=$(TA_DEV_KIT_DIR) make -C $(TA_OEMLOCK_SRC) O=$(TA_OEMLOCK_OUT) clean
-	CROSS_COMPILE=$(OPTEE_CROSS_COMPILE) BINARY=$(TA_OEMLOCK_UUID) TA_DEV_KIT_DIR=$(TA_DEV_KIT_DIR) make -C $(TA_OEMLOCK_SRC) O=$(TA_OEMLOCK_OUT) all
+include $(CLEAR_VARS)
+LOCAL_MODULE                := $(TA_OEMLOCK_UUID)
+LOCAL_MODULE_STEM           := $(TA_OEMLOCK_UUID).ta
+LOCAL_PREBUILT_MODULE_FILE  := $(TA_OEMLOCK_BINARY)
+LOCAL_MODULE_PATH           := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/optee_armtz/
+LOCAL_MODULE_CLASS          := EXECUTABLES
+include $(BUILD_PREBUILT)
 
-.PHONY: $(TA_OEMLOCK_UUID).ta
-$(TA_OEMLOCK_UUID).ta: $(TA_OEMLOCK_TARGET)
-	cp $(TA_OEMLOCK_OUT)/$@ $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/optee_armtz/$@
+$(LOCAL_BUILT_MODULE): $(TA_OEMLOCK_BINARY)
 
 endif # Include only for Renesas ones.
